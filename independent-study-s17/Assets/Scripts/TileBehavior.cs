5662f49bbff310dee954a2ac061bab024272812f
Lucas <coelho@mit.edu>
2017-02-21 18:12:58 -0500

    Tile spawner

---
+++ independent-study-s17/Assets/Scripts/TileBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehavior : MonoBehaviour {
+
+	public bool isSelected = false;
+	public bool isPlayer1Spawn = false;
+	public bool isPlayer2Spawn = false; 
+	public bool isGoal = false;
+
+}
