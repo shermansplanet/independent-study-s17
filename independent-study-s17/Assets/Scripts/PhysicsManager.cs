8f6e2244a1e82167390d6d8d55d2f4dc61770fc1
Lucas <coelho@mit.edu>
2017-03-03 01:25:02 -0500

    Physics?

--- independent-study-s17/Assets/Scripts/PhysicsManager.cs
+++ independent-study-s17/Assets/Scripts/PhysicsManager.cs
@@ -7,7 +7,8 @@ public class PhysicsManager : MonoBehaviour {
 	public double tick = 1.0;
 	public double nextTime = 0.0;
 
-
+	//remember we are in units of 2 (that's why all the random 2's)
+	public int killPlane = -8;
 
 	void Update () {
 		if (Time.time >= nextTime) {
@@ -18,6 +19,24 @@ public class PhysicsManager : MonoBehaviour {
 
 	void simulatePhysics(){
 
+		List<Pushblock> pushables =  new List<Pushblock>();
 
+		foreach (GameObject x in GameObject.FindGameObjectsWithTag("Pushblock")) {
+			if (x != null) {
+				pushables.Add (x.GetComponent<Pushblock> ());
+			}
+		}
+
+		foreach (Pushblock p in pushables) {
+			Vector3 below = new Vector3 (p.transform.position.x, p.transform.position.y - 2, p.transform.position.z);
+			if (!SpawnTiles.tileExists (below)) {
+				p.transform.Translate(Vector3.down*2);
+			}
+			if (p.transform.position.y < killPlane) {
+				SpawnTiles.blocks.Remove (SpawnTiles.roundVector (p.transform.position));
+				//pushables.Remove (p);
+				Destroy (p.gameObject);
+			}
+		}
 	}
 }
