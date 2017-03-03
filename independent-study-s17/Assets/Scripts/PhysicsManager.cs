f60a6074ea303f5b5824a114388ded445b3f91dc
Lucas <coelho@mit.edu>
2017-03-02 19:10:23 -0500

    Started working on physics

---
+++ independent-study-s17/Assets/Scripts/PhysicsManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsManager : MonoBehaviour {
+
+	public double tick = 1.0;
+	public double nextTime = 0.0;
+
+
+
+	void Update () {
+		if (Time.time >= nextTime) {
+			simulatePhysics ();
+			nextTime += tick;
+		}
+	}
+
+	void simulatePhysics(){
+
+
+	}
+}
