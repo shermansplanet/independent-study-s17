f64999d03966847f3dd48284bc8b7164c03633ba
Lucas <coelho@mit.edu>
2017-03-15 02:44:55 -0400

    Beginnings of void and ramp

--- independent-study-s17/Assets/Scripts/SpellManager.cs
+++ independent-study-s17/Assets/Scripts/SpellManager.cs
@@ -6,9 +6,9 @@ public class SpellManager : MonoBehaviour {
 
 	const float spellSpeed = 1;
 
-	public enum spell{NO_EFFECT,PUSH,DOUBLE_PUSH,CREATE_BLOCK,CREATE_PUSHBLOCK};
-	//this is currently initialized this way for testing
-	private spell[] currentSpell = new spell[]{spell.PUSH,spell.PUSH};
+	public enum spell{NO_EFFECT,PUSH,DOUBLE_PUSH,CREATE_BLOCK,CREATE_PUSHBLOCK,CREATE_VOID,CREATE_RAMP};
+	//this is currently initialized this way for testing 
+	private spell[] currentSpell = new spell[]{spell.CREATE_VOID,spell.CREATE_BLOCK};
 	public Player[] players;
 
 	public GameObject[] selectors;
@@ -17,10 +17,11 @@ public class SpellManager : MonoBehaviour {
 	//needed to instantiate new blocks
 	public GameObject tile;
 	public GameObject Pushblock;
+	public GameObject Voidblock;
+	public GameObject ramp;
 
 	private float[] spellProgress = new float[]{0,0};
 
-	// Use this for initialization
 	void Start () {
 		selectorMaterials = new Material[selectors.Length];
 		for (int i = 0; i < players.Length; i++) {
@@ -34,12 +35,11 @@ public class SpellManager : MonoBehaviour {
 			Mathf.Round (pos.y / 2),
 			Mathf.Round (pos.z / 2)) * 2;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		for (int i = 0; i < players.Length; i++) {
-			//uncomment this when we are done with testing
-			currentSpell [i] = players [i].getCurrentSpell ();
+			//uncomment line below to make use of player inventory 
+			//currentSpell [i] = players [i].getCurrentSpell ();
 			if (Input.GetButton("Spell"+i.ToString())) {
 				if (spellProgress [i] >= 1) {
 					spellProgress [i] = -1;
@@ -57,26 +57,46 @@ public class SpellManager : MonoBehaviour {
 						}
 					}
 					//PUSH and DOUBlE PUSH
-					if (currentSpell [i].Equals(spell.PUSH) && SpawnTiles.tileExists (spellPos)) {
+					if (currentSpell [i].Equals (spell.PUSH) && SpawnTiles.tileExists (spellPos)) {
 						Spellable spellableBlock = SpawnTiles.blocks [SpawnTiles.roundVector (spellPos)].GetComponent<Spellable> ();
 						if (spellableBlock != null) {
 							if (otherPlayer == -1) {
 								spellableBlock.ApplySpell (currentSpell [i], playerPos, Vector3.zero);
 							} else {
-								spellableBlock.ApplySpell (getSpellCombo(currentSpell [i],currentSpell[otherPlayer]), playerPos, getTile (players [otherPlayer].transform.position));
+								spellableBlock.ApplySpell (getSpellCombo (currentSpell [i], currentSpell [otherPlayer]), playerPos, getTile (players [otherPlayer].transform.position));
 							}
 						}
 					}
 					//CREATE BLOCK/PUSHBLOCK
-					else if (currentSpell [i].Equals(spell.CREATE_BLOCK) && !SpawnTiles.tileExists (spellPos)) {
+					else if (currentSpell [i].Equals (spell.CREATE_BLOCK) && !SpawnTiles.tileExists (spellPos)) {
 						if (otherPlayer == -1) {
-							GameObject tileClone = Instantiate(tile, spellPos, Quaternion.Euler (0,0,0));
+							GameObject tileClone = Instantiate (tile, spellPos, Quaternion.Euler (0, 0, 0));
 							SpawnTiles.blocks.Add (spellPos, tileClone);
-						} else if (getSpellCombo(currentSpell [i], currentSpell[otherPlayer]).Equals(spell.CREATE_PUSHBLOCK)) {
-							GameObject pushblockClone = Instantiate(Pushblock, spellPos, Quaternion.Euler (0,0,0));
+						} else if (getSpellCombo (currentSpell [i], currentSpell [otherPlayer]).Equals (spell.CREATE_PUSHBLOCK)) {
+							GameObject pushblockClone = Instantiate (Pushblock, spellPos, Quaternion.Euler (0, 0, 0));
 							SpawnTiles.blocks.Add (spellPos, pushblockClone);
 						}
+					} 
+					//CREAT VOID/RAMP
+					else if (currentSpell [i].Equals (spell.CREATE_VOID)) {
+						if (otherPlayer == -1) {
+							GameObject voidClone = Instantiate (Voidblock, spellPos, Quaternion.Euler (0, 0, 0));
+							//VoidManager v = (VoidManager)voidClone.GetComponent (typeof(VoidManager));
+							if (SpawnTiles.tileExists (spellPos)) {
+								//v.addObject (SpawnTiles.blocks [spellPos]);
+								Destroy (SpawnTiles.blocks[spellPos]);
+								SpawnTiles.blocks.Remove (spellPos);
+							}
+							SpawnTiles.blocks.Add (spellPos, voidClone);
+						} else if (!SpawnTiles.tileExists(spellPos) && getSpellCombo (currentSpell [i], currentSpell [otherPlayer]).Equals (spell.CREATE_RAMP)) {
+							float rotate = players [i].transform.rotation.y;
+							Debug.Log (rotate);
+							GameObject rampClone = Instantiate (ramp, new Vector3(spellPos.x,spellPos.y - 1,spellPos.z), Quaternion.Euler (-90, rotate, 0));
+							SpawnTiles.blocks.Add (spellPos, rampClone);
+						}
 					}
+
+
 
 				} else if (spellProgress [i] >= 0) {
 					spellProgress [i] += spellSpeed * Time.deltaTime;
@@ -101,6 +121,12 @@ public class SpellManager : MonoBehaviour {
 			switch (spell2) {
 			case spell.PUSH:
 				return spell.CREATE_PUSHBLOCK;
+			}
+			break;
+		case spell.CREATE_VOID:
+			switch (spell2) {
+			case spell.CREATE_BLOCK:
+				return spell.CREATE_RAMP;
 			}
 			break;
 		}
---
+++ independent-study-s17/Assets/Scripts/VoidManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidManager : MonoBehaviour {
+	
+	private List<GameObject> inventory = new List<GameObject>();
+
+	public void addObject(GameObject g) {
+		inventory.Add (g);
+	}
+
+	public GameObject getFirstObject() {
+		if (inventory.Count > 0) {
+			return inventory [0];
+		} else {
+			return null;
+		}
+	}
+
+	public void removeObject(GameObject g) {
+		if (inventory.Contains (g)) {
+			inventory.Remove (g);
+		}
+	}
+}
