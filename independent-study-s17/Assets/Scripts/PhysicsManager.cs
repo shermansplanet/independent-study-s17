041cf003d8a2cf8b100ad9f27578d2e2964ab214
Lucas Coelho <coelho@mit.edu>
2017-03-16 05:30:45 -0400

    Water pushes pushblocks

--- independent-study-s17/Assets/Scripts/PhysicsManager.cs
+++ independent-study-s17/Assets/Scripts/PhysicsManager.cs
@@ -21,6 +21,17 @@ public class PhysicsManager : MonoBehaviour {
 
 	void simulatePhysics(){
 
+		//putting this crappy annoying ridiculous code here becuase it destroys sanity
+		//just take a look at the water logic, I mean just look at it
+		//and yes I wrote this code at 4:30 am don't judge me
+		List<VoidManager> voidblocks = new List<VoidManager>();
+
+		foreach (GameObject v in GameObject.FindGameObjectsWithTag("Void")) {
+			if (v != null) {
+				voidblocks.Add (v.GetComponent<VoidManager> ());
+			}
+		}
+			
 		//Pushblock physics
 		List<Pushblock> pushables =  new List<Pushblock>();
 
@@ -55,21 +66,7 @@ public class PhysicsManager : MonoBehaviour {
 		foreach (WaterManager wtr in waterblocks) {
 			Vector3 below = new Vector3 (wtr.transform.position.x, wtr.transform.position.y - 2, wtr.transform.position.z);
 			Vector3 next = new Vector3 (0,0,0);
-			switch (wtr.getDirection ()) {
-			case 0:
-				next = new Vector3 (wtr.transform.position.x + 2, wtr.transform.position.y, wtr.transform.position.z);
-				break;
-			case 90:
-				next = new Vector3 (wtr.transform.position.x, wtr.transform.position.y, wtr.transform.position.z - 2);
-				break;
-			case 180:
-				next = new Vector3 (wtr.transform.position.x - 2, wtr.transform.position.y, wtr.transform.position.z);
-				break;
-			case 270:
-				next = new Vector3 (wtr.transform.position.x, wtr.transform.position.y, wtr.transform.position.z + 2);
-				break;
-			}
-
+			next = getNextWater (wtr);
 
 			//flow down
 			if (!SpawnTiles.tileExists (below) && below.y > killPlane) {
@@ -81,16 +78,16 @@ public class PhysicsManager : MonoBehaviour {
 				}
 				waterStream.GetComponent<WaterManager> ().changeDirection (wtr.getDirection ());
 			}
-
+				
 			//flow to next tile if free or void (that doesn't already contain water), and there is not water below
 			else if ((!SpawnTiles.tileExists (next) ||
-				SpawnTiles.blocks[SpawnTiles.roundVector(next)].GetComponent<VoidManager>() != null ||
-				!SpawnTiles.blocks[SpawnTiles.roundVector(next)].GetComponent<VoidManager>().hasObject(wtr.gameObject)) && 
+				(SpawnTiles.blocks[SpawnTiles.roundVector(next)].GetComponent<VoidManager>() != null &&
+					!SpawnTiles.blocks[SpawnTiles.roundVector(next)].GetComponent<VoidManager>().hasObject(wtr.gameObject))) && 
 				SpawnTiles.tileExists (below) && 
 				SpawnTiles.blocks[SpawnTiles.roundVector (below)].GetComponent<WaterManager>() == null) {
 				//add water to void if there is a void
 				GameObject waterStream = null;
-				if (SpawnTiles.blocks [SpawnTiles.roundVector (next)].GetComponent<VoidManager> () != null) {
+				if (SpawnTiles.tileExists(next) && SpawnTiles.blocks [SpawnTiles.roundVector (next)].GetComponent<VoidManager> () != null) {
 					waterStream = Instantiate (waterBlock, next, Quaternion.Euler (0, 0, 0));
 					SpawnTiles.blocks [SpawnTiles.roundVector (next)].GetComponent<VoidManager> ().addObject (waterStream);
 				} else {
@@ -104,17 +101,67 @@ public class PhysicsManager : MonoBehaviour {
 				waterStream.GetComponent<WaterManager> ().changeDirection (wtr.getDirection ());
 			}
 
-			//destroy if too low
+			//push pushblock
+			if (SpawnTiles.tileExists (next) &&
+			    SpawnTiles.blocks [SpawnTiles.roundVector (next)].GetComponent<Pushblock> () != null) {
+				Pushblock p = SpawnTiles.blocks [SpawnTiles.roundVector (next)].GetComponent<Pushblock> ();
+				SpawnTiles.blocks.Remove (SpawnTiles.roundVector (p.transform.position));
+
+
+				p.transform.position = getNextWaterPush (wtr.getDirection (), p);
+
+				SpawnTiles.blocks.Add (p.transform.position, GameObject.FindGameObjectWithTag("Pushblock"));
+			}
+
+			//destroy water if too low
 			if (wtr.transform.position.y < killPlane) {
 				SpawnTiles.blocks.Remove (SpawnTiles.roundVector (wtr.transform.position));
 				Destroy (wtr.gameObject);
 			}
 
-			//stop if parent is destoryed
+			//stop water if parent is destoryed
 			if (!wtr.isSource () && wtr.getParent () == null) {
 				SpawnTiles.blocks.Remove (SpawnTiles.roundVector (wtr.transform.position));
 				Destroy (wtr.gameObject);
 			}
 		}
+	}
+
+	Vector3 getNextWater(WaterManager wtr) {
+		Vector3 next = new Vector3(0,0,0);
+		switch (wtr.getDirection()) {
+		case 0:
+			next = new Vector3 (wtr.transform.position.x + 2, wtr.transform.position.y, wtr.transform.position.z);
+			break;
+		case 90:
+			next = new Vector3 (wtr.transform.position.x, wtr.transform.position.y, wtr.transform.position.z - 2);
+			break;
+		case 180:
+			next = new Vector3 (wtr.transform.position.x - 2, wtr.transform.position.y, wtr.transform.position.z);
+			break;
+		case 270:
+			next = new Vector3 (wtr.transform.position.x, wtr.transform.position.y, wtr.transform.position.z + 2);
+			break;
+		}
+		return next;
+	}
+
+	Vector3 getNextWaterPush(int direction, Pushblock p) {
+		Vector3 next = new Vector3(0,0,0);
+		switch (direction) {
+		case 0:
+			next = new Vector3 (p.transform.position.x + 2, p.transform.position.y, p.transform.position.z);
+			break;
+		case 90:
+			next = new Vector3 (p.transform.position.x, p.transform.position.y, p.transform.position.z - 2);
+			break;
+		case 180:
+			next = new Vector3 (p.transform.position.x - 2, p.transform.position.y, p.transform.position.z);
+			break;
+		case 270:
+			next = new Vector3 (p.transform.position.x, p.transform.position.y, p.transform.position.z + 2);
+			break;
+		}
+		return next;
 	}
 }
